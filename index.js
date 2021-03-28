f972a4650836d48d4ae052d31d60208fc8e606cf
Bishal Chaulagain <bishalchaulagain11011@gmail.com>
2021-03-28 14:26:26 +0545

    ADDED ONE FUNCTIONALITY

--- index.js
+++ index.js
@@ -45,6 +45,13 @@ bot.on('message', async message => {
   }
 });
 
+bot.on('message',async message =>{
+
+  if(message.content==='I miss you'){
+    message.content.reply("Mero sani , I am the luckiest person in the world.I love you with all my heart and i wanna give all the happiness in the world")
+  }
+})
+
 bot.on('message', async message => {
   // Voice only works in guilds, if the message does not come from a guild,
   // we ignore it
